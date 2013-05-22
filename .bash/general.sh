e27d06988a2b7a5547fa7bf1d7bcdae65f3d8225
Everton Ribeiro <nuxlli@gmail.com>
2013-05-22 20:11:09 -0300

    Removing fnando's.

--- .bash/general.sh
+++ .bash/general.sh
@@ -1,4 +1,6 @@
-alias ll="ls -Glahs --color=auto"
+alias ..="cd .."
+alias ls="ls --color=auto"
+alias ll="ls -Glahs"
 alias psgrep="ps aux | grep -v grep | grep"
 alias ri="ri -f ansi"
 alias reload="source ~/.bash_profile"
@@ -9,12 +11,10 @@ export VISUAL=vim
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$INSTALL_DIR/bin:$INSTALL_DIR/ruby/gems/bin:$INSTALL_DIR/node/npm/bin"
 export GREP_OPTIONS="--color=auto"
 export GREP_COLOR="4;33"
-export CDPATH=.:/vagrant:/Projects:$HOME:$HOME/Projects:/Users/fnando/Projects
+export CDPATH=.:$HOME
 export HISTFILE="$HOME/.bash_history"
 export HISTSIZE=1000000
 export HISTCONTROL=ignoreboth:erasedups
-export GEM_HOME=$INSTALL_DIR/ruby/gems
-export GEM_PATH=$INSTALL_DIR/ruby/gems
 export CDHISTORY="/tmp/cd-$USER"
 export LESS="-REX"
 
@@ -34,14 +34,6 @@ set bell-style none
 
 unset MAILCHECK                           # Disable "you have mail" warning
 ulimit -S -c 0                            # Disable core dump
-
-if [ ! -f $CDHISTORY ]; then              # Only autocd when the $CDHISTORY file
-  if [ -d /Projects ]; then               # cannot be found. In this case, try
-    cd /Projects                          # to find the /Projects directory,
-  elif [ -d /vagrant ]; then              # defaulting to the vagrant directory.
-    cd /vagrant
-  fi
-fi
 
 source $HOME/.bash/bash_completion.sh
 
